b852ba8a16be6ee3f51c6ece38acae6cea5eb8a3
ukp2 <urszula.kus-porada@capgemini.com>
2022-09-02 15:22:51 +0200

    Update clothes.py

--- clothes.py
+++ clothes.py
@@ -12,7 +12,9 @@ my_cur = my_cnx.cursor()
 #execute color
 my_cur.execute("SELECT COLOR_OR_STYLE from catalog_for_website")
 colours = my_cur.fetchall()
+
 colours = pandas.dataframe(colours)
+streamlit.text(colours)
 
 streamlit.selectbox('Choose colour:', list(colours))
 
