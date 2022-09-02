43d58e7c1540b092b4625b5781f2f7fd9154485e
ukp2 <urszula.kus-porada@capgemini.com>
2022-09-02 14:22:58 +0200

    Update clothes.py

--- clothes.py
+++ clothes.py
@@ -1,18 +1,19 @@
 import streamlit
+import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.header("Zena's Amazing Catalog")
 streamlit.text('Pick a sweatsuit colour or style') 
 
 
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") # metadata
-# my_data_row_0 = my_cur.fetchone() #fetchone
 # streamlit.text("Snowflake metadata:")
 # streamlit.text(my_data_row_0)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake2"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT color_or_style from catalog_for_website")
-colour_list = my_cur.fetchone()
+colour_list = my_cur.fetchall()
 
 streamlit.multiselect("Pick colour:", list(colour_list.index),['Pink'])
