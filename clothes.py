009d489d6ac0972aeb836b2c63486cffb8491b4e
ukp2 <urszula.kus-porada@capgemini.com>
2022-09-02 15:03:16 +0200

    Update clothes.py

--- clothes.py
+++ clothes.py
@@ -15,5 +15,5 @@ colours = my_cur.fetchall()
 
 streamlit.dataframe(colours) 
 
-colours = colours.set_index('color_or_style')
+colours = colours.set_index('0')
 streamlit.multicelect("Choose colour:", list(colours.index), ['Pink'] )
