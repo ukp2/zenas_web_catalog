fcd848492a1facc89530ecdfe2c9a19801751baf
ukp2 <urszula.kus-porada@capgemini.com>
2022-09-04 21:45:23 +0200

    Update clothes.py

--- clothes.py
+++ clothes.py
@@ -48,7 +48,7 @@ sel_col = st.selectbox('Choose colour:', list(color_list))
 st.write('our warm, comf', sel_col, 'sweatuit')
 st.write('our warm, comf', sel_col2, 'sweatuit')
 
-product_desc = ('our warm, comf', sel_col, 'sweatuit')
-st.text(product_desc)
+product_desc = ('our warm, comf ' + sel_col + 'sweatuit')
+st.write(product_desc)
 
 my_cnx.close() 
